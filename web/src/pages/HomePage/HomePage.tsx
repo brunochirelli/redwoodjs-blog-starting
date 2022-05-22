12a36bc2c974efe70e8de27b4ff7ffb8117ba171
Bruno Chirelli <brunochirelli@gmail.com>
2022-05-22 20:31:02 -0300

    adicionando layouts

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,21 +1,10 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
-import { Heading } from 'theme-ui'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-
-      <Heading sx={{ color: 'red' }}>Redwood Blog</Heading>
-
-      <nav>
-        <ul>
-          <li>
-            <Link to={routes.about()}>About</Link>
-          </li>
-        </ul>
-      </nav>
+      <h1>Home</h1>
     </>
   )
 }
