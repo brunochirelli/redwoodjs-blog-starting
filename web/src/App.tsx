0144356565964a6951bb55e9d9c4687e0bda78bf
Bruno Chirelli <brunochirelli@gmail.com>
2022-05-22 20:09:30 -0300

    chapter-1/adicionando a primeira página

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,13 +4,30 @@ import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
+import 'normalize.css'
 import './index.css'
+import { Theme, ThemeProvider } from 'theme-ui'
+
+const theme: Theme = {
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: '"Avenir Next", sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#33e',
+  },
+}
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
       <RedwoodApolloProvider>
-        <Routes />
+        <ThemeProvider theme={theme}>
+          <Routes />
+        </ThemeProvider>
       </RedwoodApolloProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
