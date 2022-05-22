c01ed69b1669608e761256adeb7150aa88f865ca
Bruno Chirelli <brunochirelli@gmail.com>
2022-05-22 20:16:00 -0300

    adicionando segunda página e usando links

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -7,14 +7,15 @@ const HomePage = () => {
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <Heading sx={{ color: 'red' }}>HomePage</Heading>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <Heading sx={{ color: 'red' }}>Redwood Blog</Heading>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to={routes.about()}>About</Link>
+          </li>
+        </ul>
+      </nav>
     </>
   )
 }
