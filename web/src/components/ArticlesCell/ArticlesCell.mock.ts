0cad3e4391c7e19ccbd8ceed2aef1e05a661fa5f
Bruno Chirelli <brunochirelli@gmail.com>
2022-05-23 18:17:04 -0300

    Add Cell Articles

---
+++ web/src/components/ArticlesCell/ArticlesCell.mock.ts
@@ -0,0 +1,23 @@
+// Define your own mock data here:
+export const standard = (/* vars, { ctx, req } */) => ({
+  articles: [
+    {
+      title: 'Title 1',
+      body: 'Body 1',
+      createdAt: '2020-01-01T00:00:00.000Z',
+      id: 42,
+    },
+    {
+      title: 'Title 2',
+      body: 'Body 2',
+      createdAt: '2020-01-01T00:00:00.000Z',
+      id: 43,
+    },
+    {
+      title: 'Title 3',
+      body: 'Body 3',
+      createdAt: '2020-01-01T00:00:00.000Z',
+      id: 44,
+    },
+  ],
+})
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,11 @@
 import { MetaTags } from '@redwoodjs/web'
+import ArticlesCell from 'src/components/ArticlesCell'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-      <h1>Home</h1>
+      <ArticlesCell />
     </>
   )
 }
